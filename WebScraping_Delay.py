3a080e17b822b54fa0d47b1af54f422024f2155a
TycePark <45834989+TycePark@users.noreply.github.com>
2019-01-03 15:31:53 -0700

    Add files via upload

---
+++ WebScraping_Delay.py
@@ -0,0 +1,13 @@
+##Use this function for sleep during webscraping with Selenium to mimick human activity
+
+from scipy.stats import truncnorm
+from time import sleep
+
+def get_truncated_normal(mean=0, sd=1, low=0,upp=10):
+    return truncnorm(
+        (low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
+
+print("This is the output")
+seconds = round(get_truncated_normal(mean=6, sd=3, low=3, upp=9).rvs(),2)
+sleep(seconds)
+print("This is the next output")
