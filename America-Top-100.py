f1c2bc422c9186ebb15cf70e69d5c3ed08e71f3f
TycePark <45834989+TycePark@users.noreply.github.com>
2019-01-03 16:56:59 -0700

    Update America-Top-100.py

--- America-Top-100.py
+++ America-Top-100.py
@@ -1,8 +1,8 @@
 import requests
+import pandas as pd
+from bs4 import BeautifulSoup
 
 r = requests.get('https://www.golfdigest.com/gallery/americas-100-greatest-golf-courses-ranking')
-
-from bs4 import BeautifulSoup
 
 soup = BeautifulSoup(r.text, 'html.parser')
 results = soup.find_all('div', attrs={'class': 'listicle-item'})
@@ -12,8 +12,6 @@ for result in results:
     name = result.find('strong').text
     description = result.find('div', attrs={'class': 'listicle-caption'}).text
     records.append((name, description))
-
-import pandas as pd
 
 df = pd.DataFrame(records, columns=['Course Name', 'Description'])
 df = df.to_csv('Top-100-2019.csv', index=False, encoding='utf-8')
