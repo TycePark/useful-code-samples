6dd35904dad80b97a079cf7f61d8b0d6320415da
TycePark <45834989+TycePark@users.noreply.github.com>
2019-01-03 15:50:46 -0700

    Update WebScraping_Delay.py

--- WebScraping_Delay.py
+++ WebScraping_Delay.py
@@ -3,7 +3,7 @@
 from scipy.stats import truncnorm
 from time import sleep
 
-def get_truncated_normal(mean=0, sd=1, low=0,upp=10):
+def get_truncated_normal(mean, sd, low, upp):
     return truncnorm(
         (low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
 
