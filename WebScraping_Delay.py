ff8d73b736c7aedb148355a9e25b731cf04b5004
TycePark <45834989+TycePark@users.noreply.github.com>
2019-01-03 16:14:14 -0700

    Update WebScraping_Delay.py

--- WebScraping_Delay.py
+++ WebScraping_Delay.py
@@ -7,6 +7,5 @@ def get_truncated_normal(mean, sd, low, upp):
         (low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
 
 print("This is the output")
-seconds = round(get_truncated_normal(mean=5.5, sd=1.5, low=3, upp=8).rvs(),2)
-sleep(seconds)
+sleep(round(get_truncated_normal(mean=5.5, sd=1.5, low=3, upp=8).rvs(),2))
 print("This is the next output")
